82db532f8af907a4daa9d6c0e36b46e9778161a3
DzL <danny601598541@gmail.com>
2024-11-28 23:32:31 -0800

    debugging

--- css532_IoT/FinalProject/AlarmClock/user_behavior.py
+++ css532_IoT/FinalProject/AlarmClock/user_behavior.py
@@ -14,13 +14,11 @@ def listen_on_bed_time(sleep_at_time, to_phone_number, stop_event):
     msg_time_interval = 10
     max_notify_times = 5
     notify_times_count = 0
-    sleep_at_time_datetime = datetime.strptime(sleep_at_time, "%Y-%m-%d %H:%M:%S")
     while not stop_event.is_set():
         # check if uses is not in bed after sleep_at_time, send notification every x interval minutes
-        current_time = datetime.now()
-        print(current_time - timedelta(minutes=msg_time_interval))
-        if (current_time - timedelta(minutes=msg_time_interval) == sleep_at_time_datetime
-                and notify_times_count < max_notify_times):
+        target_time = datetime.strftime(datetime.now() - timedelta(minutes=msg_time_interval),
+                                        "%Y-%m-%d %H:%M:%S")
+        if target_time == sleep_at_time and notify_times_count < max_notify_times:
             msg_sender.send_sms(to_phone_number, str(msg_time_interval) + " past preset sleep time, hurry up")
             msg_time_interval+=msg_time_interval
             notify_times_count += 1
